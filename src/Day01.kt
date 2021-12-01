deb23e77cfde52c656bd494068af9fef56652876
slava0135 <super.novalskiy_0135@inbox.ru>
2021-12-01 11:49:28 +0300

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var lastValue = input.first().toInt()
+        for (i in 1 until input.size) {
+            val value = input[i].toInt()
+            if (value > lastValue) count++
+            lastValue = value
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val window = 3
+        var count = 0
+        for (i in window until input.size) {
+            if (input[i-window].toInt() < input[i].toInt()) count++
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
